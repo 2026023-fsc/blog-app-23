4d2ddd6868600b28de6d84ceec29a043fb39d987
Yamamoto.K <2026023@fsc-educ.com>
2026-06-04 17:16:30 +0900

    Blog 新規投稿画面作成

--- src/main/java/com/example/blog_app/BlogController.java
+++ src/main/java/com/example/blog_app/BlogController.java
@@ -2,11 +2,25 @@ package com.example.blog_app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class BlogController {
+    private final BlogRepository blogRepository;
+    private final BlogService blogService;
+
+    public BlogController(BlogRepository blogRepository, BlogService blogService){
+        this.blogRepository = blogRepository;
+        this.blogService = blogService;
+    }
+
     @GetMapping("/blogs")
     public String blogs(){
         return "blogs";
+    }
+
+    @GetMapping("/blogs/Post")
+    public String newBlog(@ModelAttribute BrogForm Brogform){
+        return "blogs/Post";
     }
 }
--- src/main/java/com/example/blog_app/BlogForm.java
+++ src/main/java/com/example/blog_app/BlogForm.java
@@ -3,4 +3,6 @@ package com.example.blog_app;
 public class BlogForm {
     private String name;
     private String title;
+    private String mainText;
+    
 }
